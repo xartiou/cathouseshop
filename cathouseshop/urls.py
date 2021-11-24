d609f0d3d7eef5022f36f61ce1d29a24110628ef
xartiou <xartiou@gmail.com>
2021-11-24 20:44:27 +0300

    чтобы Django раздавал медиафайлы на этапе разработки, нужно папку на диске MEDIA_ROOT сделать доступной по сетевому адресу MEDIA_URL

--- cathouseshop/urls.py
+++ cathouseshop/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from mainapp.views import index, products, contact, products_home, products_office, products_modern, products_classic
 
@@ -29,4 +32,9 @@ urlpatterns = [
     path('products/modern/', products_modern, name='products_modern'),
     path('products/classic/', products_classic, name='products_classic'),
 
-]
+    ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
