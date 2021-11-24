bdf4e948dc43b82db733cf90deed64f9d1d9b053
xartiou <xartiou@gmail.com>
2021-11-24 19:46:28 +0300

    Создадим модель категории товара в магазине.

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
+    def __str__(self):
+        return self.name
