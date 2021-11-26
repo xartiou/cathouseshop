aeeb55bac7cf749ec3387dd09a34a74ad1f20fcf
xartiou <xartiou@gmail.com>
2021-11-26 11:01:40 +0300

    создали команду для чтения фала json_file

--- mainapp/management/commands/fill.py
+++ mainapp/management/commands/fill.py
@@ -1,6 +1,13 @@
+import json
+
 from django.core.management.base import BaseCommand
+
+
+def load_from_json(path):
+    with open(path, 'r') as json_file:
+        return json.load(json_file)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print('catHauseShop')
+        pass
