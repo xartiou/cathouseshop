cddbf5e49b70be4f7150d4f24de63dc8ea07139e
xartiou <xartiou@gmail.com>
2021-11-26 12:08:10 +0300

    указал путь к файлам json

--- mainapp/management/commands/fill.py
+++ mainapp/management/commands/fill.py
@@ -1,10 +1,10 @@
 import json
-
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 
-def load_from_json(path):
-    with open(path, 'r') as json_file:
+def load_from_json(file_name):
+    with open(f"{settings.BASE_dir}/json/{file_name}.json", 'r') as json_file:
         return json.load(json_file)
 
 
