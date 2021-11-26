998f6f4272ae9bc19f03641a673bad49df563852
xartiou <xartiou@gmail.com>
2021-11-26 13:48:52 +0300

    написал команды для создания категорий и продуктов в базу данных с предварительной очисткой базы

--- mainapp/management/commands/fill.py
+++ mainapp/management/commands/fill.py
@@ -1,13 +1,30 @@
 import json
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from mainapp.models import ProductCategory, Product
 
 
 def load_from_json(file_name):
-    with open(f"{settings.BASE_dir}/json/{file_name}.json", 'r') as json_file:
+    with open(f"{settings.BASE_DIR}/json/{file_name}.json", 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        categories = load_from_json('categories')
+
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            ProductCategory.objects.create(**category)
+
+        products = load_from_json('products')
+
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product['category']
+            category_item = ProductCategory.objects.get(name=category_name)
+            product['category'] = category_item
+            Product.objects.create(**product)
+
+        #User.objects.create_superuser('django', password='geekbrains')
