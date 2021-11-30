e27625ed6abb56c20fc736f6b62b8c2c747b1814
xartiou <xartiou@gmail.com>
2021-11-30 15:25:02 +0300

    прописал auth в cathouseshop/urls создал authapp/urls

--- cathouseshop/urls.py
+++ cathouseshop/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', mainapp.index, name='index'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
+    path('auth/', include('authapp.urls', namespace='auth')),
 
 ]
 
