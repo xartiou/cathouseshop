b827e44b3c5cc3e97a3acc75455d3cfe97ab3dc4
xartiou <xartiou@gmail.com>
2021-11-30 16:18:45 +0300

    прописал авторизацию использовав login_form

--- authapp/views.py
+++ authapp/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
-from django.shortcuts import render
+from authapp.forms import ShopUserLoginForm
 
-
-# Create your views here.
 
 def login(request):
-    pass
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.metod == 'POST' and login_form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    context = {
+        'login_form':login_form
+    }
+
+
+        return render(request, '', context)
 
 
 def logout(request):
