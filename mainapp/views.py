c51dd7706b4034681684cd25223446177277546e
xartiou <xartiou@gmail.com>
2021-12-03 12:07:33 +0300

    Доработал контроллер products() в приложении mainapp, обработал ошибку с несуществующим pk и pk=0, передал в шаблон список объектов категорий links_menu, чтобы позже реализовать автоматическую генерацию меню, создал файл products_list.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,10 @@
 import json
 import os
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from mainapp.models import Product, ProductCategory
 
 # Create your views here.
-module_dir = os.path.dirname(__file__,)
+module_dir = os.path.dirname(__file__, )
 
 links_menu = [
     {'url': 'products', 'title': 'все'},
@@ -13,7 +13,6 @@ links_menu = [
     {'url': 'products_modern', 'title': 'модерн'},
     {'url': 'products_classic', 'title': 'классика'},
 ]
-
 
 menu = [
     {'href': 'index', 'title': 'главная'},
@@ -27,19 +26,40 @@ def index(request):
     context = {
         'products': Product.objects.all()[:4],
         'title': 'главная'}
-    return render(request, "mainapp/index.html", context)
+    return render(request, "mainapp/index.html", context=context)
 
 
 def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+
     file_path = os.path.join(module_dir, 'fixtures/products.json')
     products = json.load(open(file_path, encoding='utf-8'))
+
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all()
+            category_item = {
+                'name': 'все',
+                'pk': 0
+            }
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category__pk=pk)
+        context = {
+            'links_menu': links_menu,
+            'title': 'продукты',
+            'category': category_item,
+            'products': products_list,
+        }
+        return render(request, "mainapp/products_list.html", context=context)
+
     context = {
-        'links_menu': ProductCategory.objects.all(),
+        'links_menu': links_menu,
         'title': 'продукты',
         'products': products,
 
     }
-    return render(request, "mainapp/products.html", context)
+    return render(request, "mainapp/products.html", context=context)
 
 
 def contact(request):
