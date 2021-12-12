09e80a30b136c8c1b767662fc3afc1cf3a043508
xartiou <xartiou@gmail.com>
2021-12-12 18:48:24 +0300

    создал форму редактирования категории продуктов.

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,9 +1,23 @@
+from django import forms
 from authapp.forms import ShopUserEditForm
 from authapp.models import ShopUser
+from mainapp.models import ProductCategory
 
 
 class ShopUserAdminEditForm(ShopUserEditForm):  # форма для работы с пользователем
     class Meta:
         model = ShopUser
         fields = '__all__'
+
+
+class ProductCategoryEditForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
 
