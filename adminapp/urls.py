0ae8da50fd35e6bd3285c205544027483678f039
xartiou <xartiou@gmail.com>
2021-12-17 17:53:56 +0300

    in url ProductsListView.as_view()

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('categories/delete/<int:pk>/', admin_views.category_delete, name='category_delete'),
     # product
     path('products/create/<int:pk>/', admin_views.product_create, name='product_create'),
-    path('products/<int:pk>/', admin_views.products, name='product_list'),
+    path('products/<int:pk>/', admin_views.ProductsListView.as_view(), name='product_list'),
     path('products/detail/<int:pk>/', admin_views.product_detail, name='product_detail'),
     path('products/update/<int:pk>/', admin_views.product_update, name='product_update'),
     path('products/delete/<int:pk>/', admin_views.product_delete, name='product_delete'),
